3b47a6aae17761409ebbb098bf41094abc51ce84
Carlos.J <148714589+Carlossaurio@users.noreply.github.com>
2025-09-28 21:51:40 -0600

    Update
    pwa3

--- lib1.js
+++ lib1.js
@@ -1,31 +1,59 @@
-// lib.js
-// http://localhost/pwasd25/index.html?n=1&d=4
+// lib1.js
+// http://localhost/pwasd25/index.html?n=2&d=5
+
 const params = new URLSearchParams(window.location.search);
 const n = params.get('n');
 const d = params.get('d');
 
 class Quickchart {
-    constructor(d) {
+    constructor(n, d) {
+        this.n = n;
         this.d = d;
     }
+
     crearCadunos() {
         let cadunos = "";
-        for(var i=1; i<this.d; i++) {
-            cadunos += "1,";
+        const numD = parseInt(this.d, 10);
+        if (!isNaN(numD) && numD > 0) {
+            for (let i = 0; i < numD; i++) {
+                cadunos += "1,";
+            }
+            return cadunos.slice(0, -1);
         }
-        cadunos = cadunos.slice(0, -1);
-        return cadunos;
+        return "";
     }
+
+    crearCadenasN() {
+        let cadenasN = "";
+        const numN = parseInt(this.n, 10);
+        const numD = parseInt(this.d, 10);
+        if (!isNaN(numN) && numD > 0) {
+            for (let i = 0; i < numN; i++) {
+                cadenasN += "1/" + numD + "|";
+            }
+            return cadenasN.slice(0, -1);
+        }
+        return "1/" + numD;
+    }
+
     generarSrcImg() {
-        let url = "https://quickchart.io/chart?cht=p3&chd=t:" + this.crearCadunos()
-            + "&chs=500x250&chl=" + "1/" + this.d;
-        return url;
+        let cadunos = this.crearCadunos();
+        let cadenasN = this.crearCadenasN();
+        if (cadunos && cadenasN) {
+            let url = "https://quickchart.io/chart?cht=p3&chd=t:" + cadunos
+                + "&chs=500x250&chl=" + cadenasN;
+            return url;
+        }
+        return "";
     }
 }
 
-// const q = new Quickchart(d); error
-let q = new Quickchart(d);
-document.getElementById("contenido").innerHTML = '<img src="' + q.generarSrcImg() + '" />';
-// '<img src="https://quickchart.io/chart?cht=p3&chd=t:1,1,1,1&chs=500x250&chl=1/4">';
-// q.generarSrcImg();
-// "<h1>Adios</h1>";
+let q = new Quickchart(n, d);
+const srcUrl = q.generarSrcImg();
+const contenidoDiv = document.getElementById("contenido");
+
+if (srcUrl) {
+    contenidoDiv.innerHTML = '<img src="' + srcUrl + '" />';
+} else {
+    contenidoDiv.innerHTML = '<h1>Valores de "n" o "d" no válidos</h1>';
+}
--- sw.js
+++ sw.js
@@ -1,19 +1,37 @@
 // sw.js
-self.addEventListener(
-    'fetch',
-    function(evento){
-        // http://localhost/pwasd25/pwa2/index.jpg > unicorn.jpg
-        // http://localhost/pwasd25/pwa2/index.jpeg > utp.png
-        console.log(evento.request.url);
-        if(/\.jpg$/.test(evento.request.url)) {
-            evento.respondWith(
-                fetch('unicorn.jpg')
-            );
-        }
-        else if(/\.png$/.test(evento.request.url)) {
-            evento.respondWith(
-                fetch('utp.png')
-            );
-        }
-    }
-);
+
+var cacheName = 'pwa3-v1';
+var filesToCache = [
+  './index.html',
+  './manifest.json',
+  './lib1.js',
+  './lib2.js',
+  './hola.jpg',
+  './unicorn.jpg',
+  './utp.png',
+  './bootstrap-5.0.2-dist/css/bootstrap.min.css',
+  './bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js'
+];
+
+self.addEventListener('install', function(evento) {
+  console.log('[ServiceWorker] Instalando...');
+  evento.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Guardando recursos de la aplicación.');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(evento) {
+  console.log('[ServiceWorker] Solicitando:', evento.request.url);
+  evento.respondWith(
+    caches.match(evento.request).then(function(response) {
+      if (response) {
+        console.log('[ServiceWorker] Recurso encontrado en caché:', evento.request.url);
+        return response;
+      }
+      return fetch(evento.request);
+    })
+  );
+});
